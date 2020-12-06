d41adb8ad89de48db5e05d1c2024998f78ff8331
Jonathan Prettyman <jonathanprettyman@Jonathans-MacBook-Air.local>
2020-12-06 10:59:07 -0800

    My failed attempt at first part of puzzle03 - and LI solution
    I again mis-understood puzzle a bit (shifting to right)  Pulled in
    LI solution to part 1 - which is what I wanted to do via a reduce, but didn't
    know how.  Going to try and puzzle out second part on my own!

---
+++ src/jjp/aoc_2020/puzzle03.clj
@@ -0,0 +1,112 @@
+(ns jjp.aoc-2020.puzzle03
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def demo-input
+  (->
+   "..##.........##.........##.........##.........##.........##.......
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
+   (str/split #"\n"))
+  )
+
+(str/join (second (partition 69 (str/join demo-input))))
+
+(def input
+  (slurp (io/reader (io/resource "puzzle_input_03.txt")))
+  )
+
+(str/join demo-input)
+(count (first demo-input))
+(defn count-trees [[x y] rows]
+  (let [l (count (first rows))
+        gap (+ l x)
+        full-string (rest (str/join rows)) ;; I think we need to drop first char due to starting pos
+        part  (doall (partition gap full-string))
+        jumps (doall (map last part))
+        c (count (filter #(= \# %) jumps))
+        ]
+    ;; c
+    {:c c :g gap :j jumps}
+    )
+  )
+
+(count-trees [3 1] demo-input)
+;; => 7
+
+(def r (count-trees [3 1] input))
+
+;; help from lambdaisland - map to vector and then to map to true / false
+;; I originally misunderstood the map model and neglected that it went
+;; infinitely to the right, I just assumed I wrapped
+
+(def demo-input-l
+  "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#")
+
+(def my-map
+  (mapv (fn [row]
+          (mapv {\# true \. false} row))
+        (str/split demo-input-l #"\n")))
+
+(defn tree? [m x y]
+  (let [width (count (first m))]
+    (get-in m [y (mod x width)])))
+
+(def pos [my-map 0 0 0])
+
+(defn sled [[my-map x y trees]]
+  (let [x (+ x 3)
+        y (+ y 1)
+        tree? (tree? my-map x y)]
+    (cond
+      (nil? tree?)
+      (reduced trees)
+
+      (true? tree?)
+      [my-map x y (inc trees)]
+
+      :else
+      [my-map x y trees])))
+
+(-> pos
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    sled
+    )
+;; => #<Reduced@1025492f: 7>
+
+(defn input->map [input]
+  (mapv (fn [row]
+          (mapv {\# true \. false} row))
+        (str/split input #"\n"))
+  )
+
+@(first (drop-while
+  (complement reduced?)
+  (iterate sled [(input->map input) 0 0 0])))
