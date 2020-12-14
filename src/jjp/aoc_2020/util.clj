2657121e77e2374c1a6286c5e397b53bdacd9ccd
Jonathan Prettyman <jonathanprettyman@Jonathans-MacBook-Air.local>
2020-12-13 18:46:55 -0800

    Add in util.clj

---
+++ src/jjp/aoc_2020/util.clj
@@ -0,0 +1,16 @@
+(ns jjp.aoc-2020.util
+  (:require [clojure.java.io :as io]))
+
+(defn parse-long [l]
+  (Long/parseLong l))
+
+(defn puzzle-input [day]
+  (io/resource (format "puzzle_input_%02d.txt" day)))
+
+(defn num-resource-seq [day]
+  (with-open [rdr (io/reader (puzzle-input day))]
+    (doall (map parse-long (line-seq rdr)))))
+
+(defn re-resource-seq [day re]
+  (with-open [rdr (io/reader (puzzle-input day))]
+    (doall (map #(next (re-find re %)) (line-seq rdr)))))
